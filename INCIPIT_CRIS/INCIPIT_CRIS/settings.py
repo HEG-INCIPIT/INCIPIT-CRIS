7a27ac75a082b1e964e4ca38917c9be237c13a7c
David Nogueiras <david.nogueiras@outlook.com>
2021-04-06 11:26:40 +0200

    add of mysql database in settings

--- INCIPIT_CRIS/INCIPIT_CRIS/settings.py
+++ INCIPIT_CRIS/INCIPIT_CRIS/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'INCIPIT_CRIS.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'incipit_cris',
+        'USER': 'INCIPIT-CRIS',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
