8c2a71d2e2b24b2ac265b013e8fbac50b8e76cf7
David Nogueiras <david.nogueiras@outlook.com>
2021-05-03 10:45:43 +0200

    correction of parsing data from the triplestore

--- INCIPIT_CRIS/sparql_triplestore/triplestore_JSON_responses_parser.py
+++ INCIPIT_CRIS/sparql_triplestore/triplestore_JSON_responses_parser.py
@@ -20,13 +20,28 @@ class Triplestore_JSON_responses_parser:
 
     def parse_get_data_person(sparql_query_answer):
         loaded_json = json.loads(sparql_query_answer)['results']['bindings'][0]
-        print(json.loads(sparql_query_answer))
         dict_data = {
             'given_name': loaded_json['given_name']['value'],
             'family_name': loaded_json['family_name']['value'],
-            'email': loaded_json['email']['value'],
-            'telephone': loaded_json['telephone']['value'],
-            'description': loaded_json['description']['value']
         }
+
+        # If email has a value in triplestore, the value is inserted in dict, if not empty string is inserted
+        if(loaded_json.get('email') != None):
+            dict_data['email'] = loaded_json['email']['value']
+        else:
+            dict_data['email'] = ''
+
+        # If telephone has a value in triplestore, the value is inserted in dict, if not empty string is inserted
+        if(loaded_json.get('telephone') != None):
+            dict_data['telephone'] = loaded_json['telephone']['value']
+        else:
+            dict_data['telephone'] = ''
+
+        # If description has a value in triplestore, the value is inserted in dict, if not empty string is inserted
+        if(loaded_json.get('description') != None):
+            dict_data['description'] = loaded_json['description']['value']
+        else:
+            dict_data['description'] = ''
+
         print(dict_data)
         return dict_data
