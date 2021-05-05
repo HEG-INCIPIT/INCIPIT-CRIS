b68f0e25e759d13f8df0aaccfd3da79b96692c5b
David Nogueiras <david.nogueiras@outlook.com>
2021-05-05 10:27:35 +0200

    modification of the order of the urls

--- INCIPIT_CRIS/INCIPIT_CRIS_app/urls.py
+++ INCIPIT_CRIS/INCIPIT_CRIS_app/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('personnes/', views.persons_research),
-    path('personnes/<path:ark_request>', views.person_display, name='person_display'),
+    path('personnes/', views.persons_research, name='person_research'),
+    path('personnes/edition/<path:ark_pid>', views.person_edition_display, name='person_edition_display'),
+    path('personnes/<path:ark_pid>', views.person_display, name='person_display'),
     path('', views.index, name='index'),
 ]
