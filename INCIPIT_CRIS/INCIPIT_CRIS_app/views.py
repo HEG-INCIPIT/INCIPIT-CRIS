b17ce6610ed5ba224081427b5e14d702699e5db4
David Nogueiras <david.nogueiras@outlook.com>
2021-05-05 10:28:22 +0200

    rename of a var

--- INCIPIT_CRIS/INCIPIT_CRIS_app/views.py
+++ INCIPIT_CRIS/INCIPIT_CRIS_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponse
-from django.template import loader
+from django.urls import reverse
 from sparql_triplestore.sparql_requests.sparql_get_Person_methods import Sparql_get_Person_methods
 
 def index(request):
@@ -13,13 +13,33 @@ def persons_research(request):
 
     return render(request, 'person/display_person_results.html', context)
 
-def person_display(request, ark_request):
-    sparql_request_check_person_ark = Sparql_get_Person_methods().check_person_ark(ark_request)
+def person_display(request, ark_pid):
+    sparql_request_check_person_ark = Sparql_get_Person_methods().check_person_ark(ark_pid)
     if(sparql_request_check_person_ark):
-        data_person = Sparql_get_Person_methods().get_data_person(ark_request)
+        data_person = Sparql_get_Person_methods().get_data_person(ark_pid)
         context = {
             'data_person': data_person
         }
         return render(request, 'person/display_person_profile.html', context)
+    
+    return render(request, 'page_404.html')
+
+def person_edition_display(request, ark_pid):
+    sparql_request_check_person_ark = Sparql_get_Person_methods().check_person_ark(ark_pid)
+    if(sparql_request_check_person_ark):
+        if (request.user.is_authenticated and (request.user.ark_pid == ark_pid or request.user.is_superuser)):
+            context = {
+                'message': "Vous pouvez éditer votre profil"
+            }
+        else:
+            if(request.user.is_authenticated):
+                context = {
+                    'message': "Vous n'avez pas le droit d'éditer ce profil"
+                }
+            else:
+                context = {
+                    'message': "Connectez-vous pour modifier votre profil"
+                }
+        return render(request, 'page_info.html', context)
     
     return render(request, 'page_404.html')
