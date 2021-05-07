807213ecf3258d46f45a26f640dc476e283deb0d
David Nogueiras <david.nogueiras@outlook.com>
2021-05-07 16:44:33 +0200

    modification of url to use part of url as parameter

--- INCIPIT_CRIS/INCIPIT_CRIS_app/urls.py
+++ INCIPIT_CRIS/INCIPIT_CRIS_app/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('personnes/', views.persons_research, name='person_research'),
+    path('personnes/edition/profil/<str:part_of_profile_to_modify>/<path:ark_pid>', views.person_profile_edition_display, name='person_description_edition'),
     path('personnes/edition/<path:ark_pid>', views.person_edition_display, name='person_edition_display'),
     path('personnes/<path:ark_pid>', views.person_display, name='person_display'),
     path('', views.index, name='index'),
