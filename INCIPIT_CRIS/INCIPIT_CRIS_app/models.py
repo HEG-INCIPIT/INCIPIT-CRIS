e6e4b5fbae28042d3a08345c664ea4f0e4e0dee0
David Nogueiras <david.nogueiras@outlook.com>
2021-05-07 16:39:38 +0200

    update User model to not update data at triplestore at each save

--- INCIPIT_CRIS/INCIPIT_CRIS_app/models.py
+++ INCIPIT_CRIS/INCIPIT_CRIS_app/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.models import AbstractUser
 from arketype_API.ark import Ark
 from sparql_triplestore.sparql_requests.sparql_post_Person_methods import Sparql_post_Person_methods
+from sparql_triplestore.sparql_requests.sparql_get_Person_methods import Sparql_get_Person_methods
 
 class User(AbstractUser):
     user = models.CharField(max_length=255)
@@ -12,12 +13,34 @@ class User(AbstractUser):
     last_name = models.CharField(max_length=150, blank=False, null=False)
     ark_pid = models.CharField(blank=True, max_length=100)
 
+    # var to verify at each save of a user if those elements changed
+    __original_email = None
+    __original_first_name = None
+    __original_last_name = None
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__original_email = self.email
+        self.__original_first_name = self.first_name
+        self.__original_last_name = self.last_name
+
     def save(self, *args, **kwargs):
-        if(self.ark_pid == ''):
-            self.ark_pid = Ark().ark_creation()
-        sparql = Sparql_post_Person_methods()
-        sparql.init_person(self.ark_pid, self.first_name, self.last_name, self.email)
+        if(not Sparql_get_Person_methods().check_person_ark(self.ark_pid)):
+            if(self.ark_pid == ''):
+                self.ark_pid = Ark().ark_creation()
+            sparql = Sparql_post_Person_methods()
+            sparql.init_person(self.ark_pid, self.first_name, self.last_name, self.email)
+        if(self.email != self.__original_email):
+            Sparql_post_Person_methods().update_person_string_leaf(self.ark_pid, 'email', self.email, self.__original_email)
+        if(self.first_name != self.__original_first_name):
+            Sparql_post_Person_methods().update_person_string_leaf(self.ark_pid, 'givenName', self.first_name, self.__original_first_name)
+        if(self.last_name != self.__original_last_name):
+            Sparql_post_Person_methods().update_person_string_leaf(self.ark_pid, 'familyName', self.last_name, self.__original_last_name)
+
         super().save(*args, **kwargs)
+        self.__original_email = self.email
+        self.__original_first_name = self.first_name
+        self.__original_last_name = self.last_name
 
     def __str__(self):
         return self.ark_pid
