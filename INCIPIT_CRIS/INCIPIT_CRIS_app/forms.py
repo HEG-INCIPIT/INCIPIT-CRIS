e2f54d4ec6974eac83cd57b6e8da552bb6c82c52
David Nogueiras <david.nogueiras@outlook.com>
2021-05-10 12:19:32 +0200

    deleted unused variables and add new form

--- INCIPIT_CRIS/INCIPIT_CRIS_app/forms.py
+++ INCIPIT_CRIS/INCIPIT_CRIS_app/forms.py
@@ -1,32 +1,43 @@
 from django import forms
 from django.core.validators import RegexValidator
 
+not_quotes_regex = RegexValidator(r"""[^".{3,}]""", "Vous ne pouvez pas mettre trois guillemets de suite")
+
+##################################################
+# Person forms
+##################################################
+
 number_phone_regex = RegexValidator(r"^[0-9]*$", "Le numéro de téléphone doit contenir uniquement des chiffres")
-description_regex = RegexValidator(r"""[^".{3,}]""", "Vous ne pouvez pas mettre trois guillemets de suite")
 
 class DescriptionForm(forms.Form):
-    url_post_redirect = "description"
-    old_description = ""
-    description = forms.CharField(label='Description ', max_length=300, required=False, validators=[description_regex])
+    description = forms.CharField(label='Description ', max_length=300, required=False, validators=[not_quotes_regex])
 
     def __init__(self, *args, **kwargs):
+        # overload init function to display actual value for a field in this one
         my_arg = ''
         if 'old_description' in kwargs:
             my_arg = kwargs.pop('old_description')
         super().__init__(*args, **kwargs)
-        if my_arg != '':
-            self.fields['description'].initial = my_arg
+        self.fields['description'].initial = my_arg
 
 class TelephoneForm(forms.Form):
-    url_post_redirect = "telephone"
-    old_telephone = ""
     telephone = forms.CharField(label='Telephone ', max_length=18, required=False, validators=[number_phone_regex])
 
     def __init__(self, *args, **kwargs):
+        # overload init function to display actual value for a field in this one
         my_arg = ''
         if 'old_telephone' in kwargs:
             my_arg = kwargs.pop('old_telephone')
         super().__init__(*args, **kwargs)
-        if my_arg != '':
-            self.fields['telephone'].initial = my_arg
+        self.fields['telephone'].initial = my_arg
 
+
+##################################################
+# Articles forms
+##################################################
+
+class Article_creationForm(forms.Form):
+    description = forms.CharField(label='Description ', max_length=300, required=False, validators=[not_quotes_regex])
+    ark_id = forms.CharField(label='Ark', max_length=100, required=False, validators=[not_quotes_regex])
+
+    
