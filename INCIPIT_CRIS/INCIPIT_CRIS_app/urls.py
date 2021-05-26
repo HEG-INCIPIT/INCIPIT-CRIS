10d3296321ae1d8ff33f060f71753d0fdf6dfc5d
David Nogueiras <david.nogueiras@outlook.com>
2021-05-26 10:59:01 +0200

    add url to delete article

--- INCIPIT_CRIS/INCIPIT_CRIS_app/urls.py
+++ INCIPIT_CRIS/INCIPIT_CRIS_app/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+
     path('personnes/', views.persons_research, name='person_research'),
     path('personnes/edition/profil/deleteArticle/<path:ark_pid>', views.person_article_deletion, name='person_article_deletion'),
     path('personnes/edition/profil/<str:part_of_profile_to_modify>/<path:ark_pid>', views.person_profile_edition_display, name='person_description_edition'),
@@ -14,6 +15,7 @@ urlpatterns = [
     path('articles/creation/', views.article_creation, name='article_creation'),
     path('articles/edition/field/addAuthor/<path:ark_pid>', views.article_author_addition, name='article_author_addition'),
     path('articles/edition/field/deleteAuthor/<path:ark_pid>', views.article_author_deletion, name='article_author_deletion'),
+    path('articles/edition/field/deleteArticle/<path:ark_pid>', views.article_deletion, name='article_deletion'),
     path('articles/edition/field/<str:part_of_article_to_edit>/<path:ark_pid>', views.article_field_edition, name='article_field_edition'),
     path('articles/edition/<path:ark_pid>', views.article_edition, name='article_edition'),
     path('articles/<path:ark_pid>', views.article_display, name='article_display'),
