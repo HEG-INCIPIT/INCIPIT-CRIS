68fef6b221ddf58d1c13f25737397295a8ddbbe3
David Nogueiras <david.nogueiras@outlook.com>
2021-07-12 11:33:29 +0200

    add urls for dataset

--- INCIPIT_CRIS/INCIPIT_CRIS_app/urls.py
+++ INCIPIT_CRIS/INCIPIT_CRIS_app/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     path('articles/edition/field/delete-author/<path:ark_pid>', views_article.article_author_deletion, name='article_author_deletion'),
     path('articles/edition/field/add-project/<path:ark_pid>', views_article.article_project_addition, name='article_project_addition'),
     path('articles/edition/field/delete-project/<path:ark_pid>', views_article.article_project_deletion, name='article_project_deletion'),
-    path('articles/edition/field/delete-article/<path:ark_pid>', views_article.article_deletion, name='article_deletion'),
+    path('articles/edition/delete-article/<path:ark_pid>', views_article.article_deletion, name='article_deletion'),
     path('articles/edition/field/<str:part_of_article_to_edit>/<path:ark_pid>', views_article.article_field_edition, name='article_field_edition'),
     path('articles/edition/<path:ark_pid>', views_article.article_edition, name='article_edition'),
     path('articles/<path:ark_pid>', views_article.article_profile, name='article_profile'),
@@ -29,7 +29,7 @@ urlpatterns = [
     path('projects/creation/', views_project.project_creation, name='project_creation'),
     path('projects/edition/field/add-member/<path:ark_pid>', views_project.project_member_addition, name='project_member_addition'),
     path('projects/edition/field/delete-member/<path:ark_pid>', views_project.project_member_deletion, name='project_member_deletion'),
-    path('projects/edition/field/delete-project/<path:ark_pid>', views_project.project_deletion, name='project_deletion'),
+    path('projects/edition/delete-project/<path:ark_pid>', views_project.project_deletion, name='project_deletion'),
     path('projects/edition/field/add-article/<path:ark_pid>', views_project.project_article_addition, name='project_article_addition'),
     path('projects/edition/field/delete-article/<path:ark_pid>', views_project.project_article_deletion, name='project_article_deletion'),
     path('projects/edition/field/<str:part_of_project_to_edit>/<path:ark_pid>', views_project.project_field_edition, name='project_field_edition'),
@@ -38,5 +38,8 @@ urlpatterns = [
 
     path('datasets/', views_dataset.dataset_results, name='dataset_results'),
     path('datasets/creation/', views_dataset.dataset_creation, name='dataset_creation'),
+    path('datasets/edition/delete-dataset/<path:ark_pid>', views_dataset.dataset_deletion, name='dataset_deletion'),
+    path('datasets/edition/<path:ark_pid>', views_dataset.dataset_edition, name='dataset_edition'),
+    path('datasets/<path:ark_pid>', views_dataset.dataset_profile, name='dataset_profile'),
     
 ]
