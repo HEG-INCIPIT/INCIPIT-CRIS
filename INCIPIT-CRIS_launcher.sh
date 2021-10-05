4a7a1d0dbd65c1b28a4f480303fcbc91f6f8a506
David Nogueiras <david.nogueiras@outlook.com>
2021-10-05 10:06:02 +0200

    Modify launcher script to be compatible with linux

--- INCIPIT-CRIS_launcher.sh
+++ INCIPIT-CRIS_launcher.sh
@@ -1,24 +1,40 @@
 #!/usr/bin/env bash
 
-exit_script() {
-    kill `ps -ef | grep 'java.*fuseki' | grep -v grep | awk '{ print $2 }'`
-    kill $FIND_PID
-}
+OS="`uname`"
 
-trap exit_script SIGTERM SIGINT
+if [ "$OS" == "Darwin" ]
+then
 
-cd fuseki/
-./fuseki-server &
+    exit_script() {
+        kill `ps -ef | grep 'java.*fuseki' | grep -v grep | awk '{ print $2 }'`
+        kill $FIND_PID
+    }
 
-cd ..
+    trap exit_script SIGTERM SIGINT
 
-if [ -d "env/" ]; then
-    . env/bin/activate
-else
-    virtualenv -p python3 env
-    pip install -r requirements.txt
+    cd fuseki/
+    ./fuseki-server &
+
+    cd ..
+
+    if [ -d "env/" ]; then
+        . env/bin/activate
+    else
+        virtualenv -p python3 env
+        pip install -r requirements.txt
+    fi
+elif [ "$OS" == "Linux" ]
+then
+    kill $(pgrep java)
+    trap exit_script SIGTERM SIGINT
+
+
+    cd fuseki
+    ./fuseki-server &
+
+    cd ..
+    source env/bin/activate
+
 fi
 
-
-python3 INCIPIT_CRIS/manage.py runserver
-
+python3 INCIPIT_CRIS/manage.py runserver 0.0.0.0:8000
