5202da24fac3ce8e643479b2f0050a976be2f725
David Nogueiras <david.nogueiras@outlook.com>
2021-10-06 11:01:10 +0200

    dockerization of the app

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM openjdk:11-jre-slim-buster
+
+# Installation of dependencies
+RUN apt-get update && apt-get upgrade -y
+RUN apt-get install grep procps -y
+RUN apt-get install python3.7 python3-pip -y
+RUN apt-get install default-mysql-server -y
+RUN apt-get install default-libmysqlclient-dev python-dev -y
+
+RUN pip3 install virtualenv
+
+# Copy essential files to docker env
+COPY fuseki/ /app/fuseki
+COPY INCIPIT_CRIS/ /app/INCIPIT_CRIS
+COPY schema_ontology/ /app/schema_ontology
+COPY INCIPIT-CRIS_docker_launcher.sh /app/INCIPIT-CRIS_docker_launcher.sh
+COPY requirements.txt /app/requirements.txt
+
+WORKDIR /app
+
+EXPOSE 8000
+
+CMD ./INCIPIT-CRIS_docker_launcher.sh
---
+++ INCIPIT-CRIS_docker_launcher.sh
@@ -0,0 +1,33 @@
+echo "START"
+service mysql start
+mysql -e "CREATE DATABASE incipit_cris;"
+mysql -e "CREATE USER 'INCIPIT-CRIS'@'localhost' IDENTIFIED BY 'password';"
+mysql -e "GRANT ALL ON *.* TO 'INCIPIT-CRIS'@'localhost';"
+
+echo "RUN FUSEKI"
+
+cd fuseki
+./fuseki-server & >> fuseki.log
+
+cd ..
+
+echo "PYTHON"
+
+if [ -d "env/" ]; then
+    echo "ACTIVATE ENV"
+    source env/bin/activate
+else
+    echo "VIRTUAL ENV"
+    virtualenv -p python3.7 env
+    echo "ACTIVATE ENV"
+    source env/bin/activate
+    echo "REQUIREMENTS"
+    pip3 install -r requirements.txt
+    echo "SETUP DJANGO ENVIRONEMENT"
+    python3.7 INCIPIT_CRIS/manage.py migrate
+    DJANGO_SUPERUSER_USERNAME=admin DJANGO_SUPERUSER_PASSWORD=pw DJANGO_SUPERUSER_EMAIL=admin@incipit-cris.com python3.7 INCIPIT_CRIS/manage.py createsuperuser --noinput
+    echo "INSERTING SCHEMA.ORG ONTOLOGY INTO CRIS"
+    python3.7 INCIPIT_CRIS/manage.py add_schema_to_cris
+fi
+echo "RUN SERVER"
+python3.7 INCIPIT_CRIS/manage.py runserver 0.0.0.0:8000 --noreload
