0ebdae49dbf14fe1dd06c3549887f3c31457bcca
David Nogueiras <david.nogueiras@outlook.com>
2021-11-24 16:32:29 +0100

    fix bug

--- INCIPIT_CRIS/INCIPIT_CRIS_app/views_institution.py
+++ INCIPIT_CRIS/INCIPIT_CRIS_app/views_institution.py
@@ -140,7 +140,7 @@ def institution_profile(request, pid):
         data_institution = variables.sparql_get_institution_object.get_data_institution(pid)
         can_edit = True if request.user.is_authenticated and (request.user.pid == pid or request.user.is_superuser) else False
         edition_granted = False
-        if request.user.is_superuser or request.user.is_authenticated and request.user.pid in [members[0] for members in data_institution['members']]:
+        if request.user.is_superuser:
             edition_granted = True
         context = {
             'can_edit': can_edit,
