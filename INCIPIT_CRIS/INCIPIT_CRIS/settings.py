b46871625dd4dfefb95c15b4c9da6c6c831f323b
Amir Alwash <amir@alwash.ch>
2021-12-01 12:45:09 +0100

    Remove COS handling

--- INCIPIT_CRIS/INCIPIT_CRIS/settings.py
+++ INCIPIT_CRIS/INCIPIT_CRIS/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'cookiebanner',
-    'corsheaders',
 ]
 COOKIEBANNER = {
     "title": "Réglages des cookies",
@@ -82,10 +81,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True
 ROOT_URLCONF = 'INCIPIT_CRIS.urls'
 
 TEMPLATES = [
