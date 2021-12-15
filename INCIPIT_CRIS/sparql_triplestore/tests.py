9f88b3b2fbea1fd66187126290e7de1490c3906f
David Nogueiras <david.nogueiras@outlook.com>
2021-12-15 15:39:58 +0100

    create new class to test dataset parser

--- INCIPIT_CRIS/sparql_triplestore/tests.py
+++ INCIPIT_CRIS/sparql_triplestore/tests.py
@@ -1,29 +1,27 @@
 from django.test import TestCase
 from .triplestore_JSON_parser.triplestore_JSON_parser_generic import *
 from .triplestore_JSON_parser.triplestore_JSON_parser_article import *
+from .triplestore_JSON_parser.triplestore_JSON_parser_dataset import *
 
 # Create your tests here.
 
 class GenericJSONParser(TestCase):
-    #######################################################################################################################################
-    # Creating json variables to test parser
-    #######################################################################################################################################
+    
+    def setUp(self):
+        # parse_get_simple_elements_article
+        self.get_authors_article = b'{ "head": {\n    "vars": [ "author" ]\n  } ,\n  "results": {\n    "bindings": [\n \
+            { \n        "author": { "type": "uri" , "value": "ark:/99999/1" }\n      } ,\n \
+            { \n        "author": { "type": "uri" , "value": "ark:/99999/2" }\n      } ,\n \
+            { \n        "author": { "type": "uri" , "value": "ark:/99999/3" }\n      }\n    ]\n  }\n}\n'
 
-    # parse_get_simple_elements_article
+        self.get_authors_article_empty = b'{ "head": {\n    "vars": [ "author" ]\n  } ,\n  "results": {\n    "bindings": [\n \n]\n  }\n}\n'
 
-    get_authors_article = b'{ "head": {\n    "vars": [ "author" ]\n  } ,\n  "results": {\n    "bindings": [\n \
-        { \n        "author": { "type": "uri" , "value": "ark:/99999/1" }\n      } ,\n \
-        { \n        "author": { "type": "uri" , "value": "ark:/99999/2" }\n      } ,\n \
-        { \n        "author": { "type": "uri" , "value": "ark:/99999/3" }\n      }\n    ]\n  }\n}\n'
+        self.get_projects_article = b'{ "head": {\n    "vars": [ "project" ]\n  } ,\n  "results": {\n    "bindings": [\n \
+            { \n        "project": { "type": "uri" , "value": "ark:/99999/1" }\n      } ,\n \
+            { \n        "project": { "type": "uri" , "value": "ark:/99999/2" }\n      } ,\n \
+            { \n        "project": { "type": "uri" , "value": "ark:/99999/3" }\n      }\n    ]\n  }\n}\n'
 
-    get_authors_article_empty = b'{ "head": {\n    "vars": [ "author" ]\n  } ,\n  "results": {\n    "bindings": [\n \n]\n  }\n}\n'
-
-    get_projects_article = b'{ "head": {\n    "vars": [ "project" ]\n  } ,\n  "results": {\n    "bindings": [\n \
-        { \n        "project": { "type": "uri" , "value": "ark:/99999/1" }\n      } ,\n \
-        { \n        "project": { "type": "uri" , "value": "ark:/99999/2" }\n      } ,\n \
-        { \n        "project": { "type": "uri" , "value": "ark:/99999/3" }\n      }\n    ]\n  }\n}\n'
-
-    get_projects_article_empty = b'{ "head": {\n    "vars": [ "project" ]\n  } ,\n  "results": {\n    "bindings": [\n \n]\n  }\n}\n'
+        self.get_projects_article_empty = b'{ "head": {\n    "vars": [ "project" ]\n  } ,\n  "results": {\n    "bindings": [\n \n]\n  }\n}\n'
 
 
     def test_article_JSON_parser(self):
@@ -36,41 +34,38 @@ class GenericJSONParser(TestCase):
 
 
 class ArticleJSONParser(TestCase):
-    #######################################################################################################################################
-    # Creating json variables to test parser
-    #######################################################################################################################################
-    
-    # parse_get_articles
 
-    get_articles = b'{ "head": {\n    "vars": [ "article" , "name" ]\n  } ,\n "results": { \n    "bindings": [\n \
-        { \n        "article": { "type": "uri" , "value": "ark:/99999/test1" } ,\n \
-        "name": { "type": "literal" , "value": "Name test 1" }\n      } ,\n \
-        { \n        "article": { "type": "uri" , "value": "ark:/99999/test2" } ,\n \
-        "name": { "type": "literal" , "value": "Name test 2" }\n      }\n    ]\n  }\n}\n'
-    
-    get_articles_empty = b'{ "head": {\n    "vars": [ "article" , "name" ]\n  } ,\n "results": { \n    "bindings": [\n     \n]\n  }\n}\n'
+    def setUp(self):
+        # parse_get_articles
+        self.get_articles = b'{ "head": {\n    "vars": [ "article" , "name" ]\n  } ,\n "results": { \n    "bindings": [\n \
+            { \n        "article": { "type": "uri" , "value": "ark:/99999/test1" } ,\n \
+            "name": { "type": "literal" , "value": "Name test 1" }\n      } ,\n \
+            { \n        "article": { "type": "uri" , "value": "ark:/99999/test2" } ,\n \
+            "name": { "type": "literal" , "value": "Name test 2" }\n      }\n    ]\n  }\n}\n'
+        
+        self.get_articles_empty = b'{ "head": {\n    "vars": [ "article" , "name" ]\n  } ,\n "results": { \n    "bindings": [\n     \n]\n  }\n}\n'
 
-    # parse_check_article_ark
+        # parse_check_article_ark
 
-    check_article_true = b'{ "head": {\n    "vars": [ "scholarlyArticle" ]\n  } ,\n  "results": {\n    "bindings": [\n \
-        { \n        "scholarlyArticle": { "type": "uri" , "value": "https://schema.org/ScholarlyArticle" }\n      }\n    ]\n  }\n}\n'
+        self.check_article_true = b'{ "head": {\n    "vars": [ "scholarlyArticle" ]\n  } ,\n  "results": {\n    "bindings": [\n \
+            { \n        "scholarlyArticle": { "type": "uri" , "value": "https://schema.org/ScholarlyArticle" }\n      }\n    ]\n  }\n}\n'
 
-    check_article_false = b'{ "head": {\n    "vars": [ "scholarlyArticle" ]\n  } ,\n  "results": {\n    "bindings": [\n    \n]\n  }\n}\n'
+        self.check_article_false = b'{ "head": {\n    "vars": [ "scholarlyArticle" ]\n  } ,\n  "results": {\n    "bindings": [\n    \n]\n  }\n}\n'
 
-    # parse_get_data_article
-    get_data_article = b'{ "head": {\n    "vars": [ "name" , "abstract" , "datePublished" , "url" ]\n  } ,\n  "results": {\n    "bindings": [\n \
-        { \n        "name": { "type": "literal" , "value": "Article name test" } ,\
-        \n        "abstract": { "type": "literal" , "value": "Article abstract test" } ,\
-        \n        "datePublished": { "type": "literal" , "datatype": "http://www.w3.org/2001/XMLSchema#date" , "value": "2022-01-01 00:00:00+00:00" } ,\
-        \n        "url": { "type": "literal" , "value": "www.url.ch" }\n      }\n    ]\n  }\n}\n'
+        # parse_get_data_article
+        self.get_data_article = b'{ "head": {\n    "vars": [ "name" , "abstract" , "datePublished" , "url" ]\n  } ,\n  "results": {\n    "bindings": [\n \
+            { \n        "name": { "type": "literal" , "value": "Article name test" } ,\
+            \n        "abstract": { "type": "literal" , "value": "Article abstract test" } ,\
+            \n        "datePublished": { "type": "literal" , "datatype": "http://www.w3.org/2001/XMLSchema#date" , "value": "2022-01-01 00:00:00+00:00" } ,\
+            \n        "url": { "type": "literal" , "value": "www.url.ch" }\n      }\n    ]\n  }\n}\n'
 
-    get_data_article_empty = b'{ "head": {\n    "vars": ["name" , "abstract" , "datePublished" , "url"]\n  } ,\n  "results": {\n    "bindings": [\n ]\n }\n}\n'
+        self.get_data_article_empty = b'{ "head": {\n    "vars": ["name" , "abstract" , "datePublished" , "url"]\n  } ,\n  "results": {\n    "bindings": [\n ]\n }\n}\n'
 
-    # parse_get_DOI_information
-    get_DOI_information = b'{ "head": {\n    "vars": [ "value" ]\n  } ,\n  "results": {\n    "bindings": [\n \
-        { \n        "value": { "type": "literal" , "value": "10.1" }\n      }\n    ]\n  }\n}\n'
-    
-    get_DOI_information_empty = b'{ "head": {\n    "vars": [ "value" ]\n  } ,\n  "results": {\n    "bindings": [\n    \n]\n  }\n}\n'
+        # parse_get_DOI_information
+        self.get_DOI_information = b'{ "head": {\n    "vars": [ "value" ]\n  } ,\n  "results": {\n    "bindings": [\n \
+            { \n        "value": { "type": "literal" , "value": "10.1" }\n      }\n    ]\n  }\n}\n'
+        
+        self.get_DOI_information_empty = b'{ "head": {\n    "vars": [ "value" ]\n  } ,\n  "results": {\n    "bindings": [\n    \n]\n  }\n}\n'
 
 
     def test_article_JSON_parser(self):
@@ -90,3 +85,30 @@ class ArticleJSONParser(TestCase):
         self.assertEqual(parse_get_DOI_information(self.get_DOI_information), '10.1')
         self.assertEqual(parse_get_DOI_information(self.get_DOI_information_empty), '')
 
+
+class DatasetJSONParser(TestCase):
+    
+    def setUp(self):
+        # parse_get_simple_elements_article
+        self.get_authors_article = b'{ "head": {\n    "vars": [ "author" ]\n  } ,\n  "results": {\n    "bindings": [\n \
+            { \n        "author": { "type": "uri" , "value": "ark:/99999/1" }\n      } ,\n \
+            { \n        "author": { "type": "uri" , "value": "ark:/99999/2" }\n      } ,\n \
+            { \n        "author": { "type": "uri" , "value": "ark:/99999/3" }\n      }\n    ]\n  }\n}\n'
+
+        self.get_authors_article_empty = b'{ "head": {\n    "vars": [ "author" ]\n  } ,\n  "results": {\n    "bindings": [\n \n]\n  }\n}\n'
+
+        self.get_projects_article = b'{ "head": {\n    "vars": [ "project" ]\n  } ,\n  "results": {\n    "bindings": [\n \
+            { \n        "project": { "type": "uri" , "value": "ark:/99999/1" }\n      } ,\n \
+            { \n        "project": { "type": "uri" , "value": "ark:/99999/2" }\n      } ,\n \
+            { \n        "project": { "type": "uri" , "value": "ark:/99999/3" }\n      }\n    ]\n  }\n}\n'
+
+        self.get_projects_article_empty = b'{ "head": {\n    "vars": [ "project" ]\n  } ,\n  "results": {\n    "bindings": [\n \n]\n  }\n}\n'
+
+
+    def test_article_JSON_parser(self):
+        # parse_get_simple_elements_article
+        self.assertEqual(parse_get_simple_elements_article(self.get_authors_article, 'author'), ['ark:/99999/1', 'ark:/99999/2', 'ark:/99999/3'])
+        self.assertEqual(parse_get_simple_elements_article(self.get_authors_article_empty, 'author'), [])
+
+        self.assertEqual(parse_get_simple_elements_article(self.get_projects_article, 'project'), ['ark:/99999/1', 'ark:/99999/2', 'ark:/99999/3'])
+        self.assertEqual(parse_get_simple_elements_article(self.get_projects_article_empty, 'project'), [])
