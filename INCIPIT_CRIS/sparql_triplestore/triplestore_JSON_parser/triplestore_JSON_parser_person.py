2048ca38c88d464c1edbdfaabd430e515419b28a
David Nogueiras <david.nogueiras@outlook.com>
2021-12-22 15:29:17 +0100

    remove a print

--- INCIPIT_CRIS/sparql_triplestore/triplestore_JSON_parser/triplestore_JSON_parser_person.py
+++ INCIPIT_CRIS/sparql_triplestore/triplestore_JSON_parser/triplestore_JSON_parser_person.py
@@ -43,7 +43,6 @@ def parse_get_IN_information(sparql_query_answer):
 
 
 def parse_get_data_person(sparql_query_answer):
-    print(sparql_query_answer)
     loaded_json = json.loads(sparql_query_answer)['results']['bindings'][0]
     dict_data = {
         'given_name': loaded_json['given_name']['value'],
