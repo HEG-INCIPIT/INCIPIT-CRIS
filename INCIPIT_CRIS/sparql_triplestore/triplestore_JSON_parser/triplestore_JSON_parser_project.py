c0f9181262917d1c683631cbdc8952fb63468b39
David Nogueiras <david.nogueiras@outlook.com>
2022-02-17 11:13:07 +0100

    Fix wrong paths, correct display of institution or funder if they don't have an alternate name and change logo for funder when there are no logo

--- INCIPIT_CRIS/sparql_triplestore/triplestore_JSON_parser/triplestore_JSON_parser_project.py
+++ INCIPIT_CRIS/sparql_triplestore/triplestore_JSON_parser/triplestore_JSON_parser_project.py
@@ -2,14 +2,17 @@ import json
 
 
 def parse_get_data_project(sparql_query_answer):
-    loaded_json = json.loads(sparql_query_answer)['results']['bindings'][0]
-    dict_data = {
-        'name': loaded_json['name']['value'],
-        'description': loaded_json['description']['value'],
-        'founding_date': loaded_json['foundingDate']['value'][:10],
-        'dissolution_date': loaded_json['dissolutionDate']['value'][:10],
-        'url': loaded_json['url']['value'],
-        'logo': loaded_json['logo']['value'] if 'logo' in loaded_json else '',
-    }
+    if(len(json.loads(sparql_query_answer)['results']['bindings']) > 0):
+        loaded_json = json.loads(sparql_query_answer)['results']['bindings'][0]
+        dict_data = {
+            'name': loaded_json['name']['value'],
+            'description': loaded_json['description']['value'],
+            'founding_date': loaded_json['foundingDate']['value'][:10],
+            'dissolution_date': loaded_json['dissolutionDate']['value'][:10],
+            'url': loaded_json['url']['value'],
+            'logo': loaded_json['logo']['value'] if 'logo' in loaded_json else '',
+        }
 
-    return dict_data
+        return dict_data
+    
+    return {}
